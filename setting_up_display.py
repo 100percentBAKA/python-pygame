c39b47ee6a9491ee8821616c66c6e99517a02f98
ad4r5hgs <11rn211cs011.adarshgs>
2023-07-08 13:43:04 +0530

    Update setting_up_display.py

--- setting_up_display.py
+++ setting_up_display.py
@@ -5,23 +5,28 @@ pygame.init()
 
 # setting up the display window 
 WIDTH, HEIGHT = 700, 700
+FPS = 60
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hello PyGame")
 
+def render_surface():
+    WIN.fill((0,0,0)) ### black
+    pygame.display.flip()
+
 # main game loop
 def main():
-
     run = True
+    
+    clock = pygame.time.Clock()
     while run: 
+        clock.tick(FPS) ### setting the frame rate
         ## handling events 
         for events in pygame.event.get(): ### pygame.event.get() returns a list 
             if events.type == pygame.QUIT: 
                 run = False
-        
-
-        # screen rendering 
-        WIN.fill((0, 0, 0))
-        pygame.display.update() ### updates the screen
+    
+        render_surface()
 
 
 # calling the main function to start the game
